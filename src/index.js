d3576652819057ce0483b05aa8461975b4606090
Evgen Skaskevich <skaskevich23@gmail.com>
2018-05-08 16:33:50 +0300

    config_project, connect_redux_route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import './main.css';
-import { createStore } from 'redux';
 
-const store = createStore();
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-document.write('Hello React/Redux!')
+import { createStore, applyMiddleware } from 'redux';
+import reducers from 'reducers';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { Provider } from 'react-redux'
+import { Router, Route } from 'react-router';
+import Layout from 'containers/layout';
+import Phones from 'containers/phones';
+
+const store = createStore( reducers, composeWithDevTools(
+	applyMiddleware(thunk)
+));
+
+const history = syncHistoryWithStore( browserHistory, store)
+
+ReactDOM.render(
+	<Provider store={store}>
+		<Router history={history}>
+			<Route component={Layout}>
+				<Route path="/" component={Phones} />
+			</Route>
+		</Router>
+	</Provider>,
+	document.getElementById('root')
+)
